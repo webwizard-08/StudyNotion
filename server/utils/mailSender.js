c9b0944ebe5f840dc527a05279240c421200e511
webwizard-08 <adarshyadav8804@gmail.com>
2026-07-30 18:57:51 +0530

    Fix SMTP IPv4

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,12 +3,17 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
+  host: process.env.MAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  family: 4, // Force IPv4
+});
+await transporter.verify();
+console.log("SMTP Connected Successfully");
 
     const info = await transporter.sendMail({
       from: `StudyNotion <${process.env.MAIL_USER}>`,
