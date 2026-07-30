867ce78ae66303d0e942e752ae881344540d4118
webwizard-08 <adarshyadav8804@gmail.com>
2026-07-30 19:52:02 +0530

    Fix mailSender import

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mailSender = require("../utils/mailSender");
+const { mailSender } = require("../utils/mailSender");
 const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 const OTPSchema = new mongoose.Schema({
 	email: {
