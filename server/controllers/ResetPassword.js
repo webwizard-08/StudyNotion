650798eeaa7cc58c598641b223b1f4b2bc26815a
webwizard-08 <adarshyadav8804@gmail.com>
2026-07-31 00:52:37 +0530

    Fix mailSender import in reset password controller

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const mailSender = require("../utils/mailSender");
+const { mailSender } = require("../utils/mailSender");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
