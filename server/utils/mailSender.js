501690347d59404f24e33ac65983c4a649029d77
webwizard-08 <adarshyadav8804@gmail.com>
2026-07-30 19:42:24 +0530

    Switch Gmail SMTP to Brevo SMTP

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,39 +1,28 @@
 const nodemailer = require("nodemailer");
 
-const mailSender = async (email, title, body) => {
+exports.mailSender = async (email, title, body) => {
   try {
-    console.log("Creating transporter...");
-
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true,
-      family: 4,
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
+      port: 587,
+      secure: false, // 587 ke liye false
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
     });
 
-    console.log("Transporter created");
-
     const info = await transporter.sendMail({
-      from: process.env.MAIL_USER,
+      from: `"StudyNotion" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("Mail Sent", info);
-
+    console.log("Email sent:", info.messageId);
     return info;
-  } catch (err) {
-    console.error("Mail Error:", err);
-    throw err;
+  } catch (error) {
+    console.log("Mail Error:", error);
+    throw error;
   }
 };
-
-module.exports = mailSender;
