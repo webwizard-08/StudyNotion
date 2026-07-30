66a68d0c83da9559a3b8f896cd9d316a70f74f32
webwizard-08 <adarshyadav8804@gmail.com>
2026-07-30 16:39:03 +0530

    Fix TimelineImage import case

--- src/components/core/HomePage/TimelineSection.jsx
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -4,7 +4,7 @@ import Logo1 from "../../../assets/TimeLineLogo/Logo1.svg";
 import Logo2 from "../../../assets/TimeLineLogo/Logo2.svg";
 import Logo3 from "../../../assets/TimeLineLogo/Logo3.svg";
 import Logo4 from "../../../assets/TimeLineLogo/Logo4.svg";
- import timelineImage from "../../../assets/Images/timelineImage.png";
+import timelineImage from "../../../../assets/Images/TimelineImage.png";
 
 const timeline = [
   {
